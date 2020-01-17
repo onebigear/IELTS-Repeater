1184fb33a2b9ab94f73639bf7c10167df520c294
stevexyh@hotmail.com <stevexyh@hotmail.com>
2020-01-17 15:59:00 +0800

    OS path problem fixed, add pyobjc to requirements

--- Dictation.py
+++ Dictation.py
@@ -2,6 +2,8 @@ from playsound import playsound
 import time
 import os
 import random
+import platform
+
 # playsound("Speech_US\desert.mp3")
 class Dictation():
     def __init__(self):
@@ -19,12 +21,16 @@ class Dictation():
             file_object.close()
         
     def playSound(self):
+        if platform.system().lower() == 'windows':
+            os_slash = '\\'
+        else:
+            os_slash = '/'
         for word in self.wordList:
             accent = random.randint(0, 100)
             if accent%2 == 0:
-                soundPath = "Speech_US\\"
+                soundPath = "Speech_US" + os_slash
             else:
-                soundPath = "Speech_EN\\"
+                soundPath = "Speech_EN" + os_slash
             soundPath += str(word)+".mp3"
             print(str(accent%2))
             playsound(soundPath)
